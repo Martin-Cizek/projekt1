0bc3e60f265aa1a8d699cafe191741a709ae33fb
Martin-Cizek <84407033+Martin-Cizek@users.noreply.github.com>
2021-07-04 15:38:45 +0200

    Add files via upload

--- textanalyzer.py
+++ textanalyzer.py
@@ -1,6 +1,8 @@
 '''
 author = Martin Cizek 
 '''
+import string
+
 TEXTS = ['''
 Situated about 10 miles west of Kemmerer, 
 Fossil Butte is a ruggedly impressive 
@@ -32,94 +34,140 @@ garpike and stingray are also present.'''
 ]
 ODDEL = 80*'-'
 CREDENTIALS = {'bob':'123', 'ann':'pass123','mike':'password123', 'liz':'pass123'}
+#----------------------------------------------------------------------------
+#prevedeni stringu na integer s odchycenim chyby
+def str2intdef(int_str, defaultv):
+    try:
+        i=int(int_str)
+    except ValueError:
+        i=defaultv
+    return i
 
-#prihlaseni uzivatele
-login = input('Zadej jmeno uzivatele: ')
-pwd = input(f'Zadej heslo pro uzivatele {login}: ')
-print(ODDEL)
+#----------------------------------------------------------------------------
+#ocisteni stringu od jinych znaku nez jsou pismena, cislice a mezery
+def clean_text(text_in):
+    text_clean = ''.join(ch for ch in text_in 
+                        if ch in (string.digits + string.ascii_letters + ' ')).strip()
+    return text_clean
 
-#overeni uzivatele
-if not((login in CREDENTIALS.keys()) and (CREDENTIALS[login]==pwd)):    
-    print('Nespravne uzivatelske jmeno nebo heslo. Ukoncuji program.')
-    exit()
+#----------------------------------------------------------------------------
+#analyza listu slov
+def analyze_words(words_in):
+    #inicializace pocitadel
+    N_words = len(words_in)
+    N_titlecase_words = 0   #pocet slov zacinajicich velkym pismenem
+    N_uppercase_words = 0   #pocet slov psanych velkymi pismeny
+    N_lowercase_words = 0   #pocet slov psanych malymi pismeny
+    N_numbers = 0           #pocet cisel v textu
+    numbers_sum = 0         #soucet cisel v textu
+    wrd_lengths=dict()
+    #analyza textu
+    for wrd in words_in:
+        wrd_len = len(wrd)
+        wrd_lengths[wrd_len]=wrd_lengths.setdefault(wrd_len, 0)+1
+        if wrd.istitle(): 
+            N_titlecase_words += 1 
+        elif wrd.isupper(): 
+            N_uppercase_words += 1
+        elif wrd.islower():
+            N_lowercase_words += 1
+        elif wrd.isnumeric():
+            N_numbers += 1
+            numbers_sum += int(wrd)
+    
+    #serazeni slovniku delek slov od nejkratsiho po nejdelsi
+    wrd_lengths = dict(sorted(wrd_lengths.items()))
+    
+    dict_out=dict();
+    dict_out['N_words']=N_words
+    dict_out['N_titlecase_words']=N_titlecase_words
+    dict_out['N_uppercase_words']=N_uppercase_words
+    dict_out['N_lowercase_words']=N_lowercase_words
+    dict_out['N_numbers']=N_numbers
+    dict_out['numbers_sum']=numbers_sum
+    dict_out['wrd_lengths']=wrd_lengths    
+    return dict_out
+#----------------------------------------------------------------------------    
 
-#uvitani uzivatele a zobrazeni vyberu textu
-print('')
-print(f'Ahoj {login}, vitej v programu Text Analyzer!')
-print(f'Vyber si prosim jeden z nasledujicich {len(TEXTS)} ukazkovych textu k provedeni analyzy:')
-print('')
-for i in range(len(TEXTS)):
-    print(f'{i+1}: {TEXTS[i]}')
+#funkce main
+def text_analyzer_main():
+    #prihlaseni uzivatele
+    login = input('Zadej jmeno uzivatele: ')
+    pwd = input(f'Zadej heslo pro uzivatele {login}: ')
     print(ODDEL)
-    print('')
-#volba textu k analyze
-print(f'Povolene volby jsou 1 - {len(TEXTS)}. 0 - konec')
-while not((selection:=int(input('Tvuj vyber: '))) in range(1,len(TEXTS)+1)):
-    if selection==0:
-        print('Ukoncuji program.')
+    
+    #overeni uzivatele
+    if not((login in CREDENTIALS.keys()) and (CREDENTIALS[login]==pwd)):    
+        print('Nespravne uzivatelske jmeno nebo heslo. Ukoncuji program.')
         exit()
-    else:
-        print(f'Opakuj zadani. Povolene volby jsou 1 - {len(TEXTS)}')
+    
+    #uvitani uzivatele a zobrazeni vyberu textu
+    num_texts=len(TEXTS)
+    
+    print('')
+    print(f'Ahoj {login}, vitej v programu Text Analyzer!')
+    print(f'Vyber si prosim jeden z nasledujicich {num_texts} ukazkovych textu k provedeni analyzy:')
+    print('')
+    
+    for i in range(num_texts):
+        print(f'{i+1}: {TEXTS[i]}')
+        print(ODDEL)
+        print('')
+    
+    #volba textu k analyze    
+    print(f'Povolene volby jsou 1 - {num_texts}. 0 - konec')
+    while not((selection:=str2intdef(input('Tvuj vyber: '),-1)) in range(1,num_texts+1)):
+        if selection==0:
+            print('Ukoncuji program.')
+            exit()
+        else:
+            print(f'Opakuj zadani. Povolene volby jsou 1 - {num_texts}')
+    
+    print(ODDEL)
+    print(f'Analyzuji text c. {selection}...')
+    print('')
+    
+    selection-=1    
+    
+    #nacteni stringu k analyze, jeho ocisteni a rozdeleni do listu slov
+    str_clean = clean_text(TEXTS[selection])
+    
+    #rozdeleni stringu do listu slov, odeleovacem je mezera
+    words = str_clean.split(' ')
+    
+    #analyza listu slov    
+    wrd_analysis = analyze_words(words)
+        
+    #kontrolni vypisy
+    #print(f'Ocisteny string s textem:\n{str_clean}\n')
+    #print(f'List jednotlivych slov:\n{words}\n')
+    #print(wrd_analysis)
+        
+    #zobrazeni vysledku
+    print(f"Pocet slov v textu: {wrd_analysis['N_words']}")
+    print(f"Pocet slov zacinajicich velkym pismenem: {wrd_analysis['N_titlecase_words']}")
+    print(f"Pocet slov psanych velkymi pismeny: {wrd_analysis['N_uppercase_words']}")
+    print(f"Pocet slov psanych malymi pismeny: {wrd_analysis['N_lowercase_words']}")
+    print(f"Pocet cisel: {wrd_analysis['N_numbers']}")
+    print(f"Soucet vsech cisel: {wrd_analysis['numbers_sum']}\n")
+    
+    #*** Vystup do tabulky ***
+    #maximalni sirka prostredniho sloupce
+    max_count = max(wrd_analysis['wrd_lengths'].values())
+    #format radku tabulky
+    fmt_string_row= "{:>4} {:<" + str(max_count) + "} {:<4}"
+    #format zahlavi tabulky
+    fmt_string_hdr= "{:>4} {:^" + str(max_count) + "} {:<4}"
+    #vypis zahlavi a radku tabulky
+    print(ODDEL)
+    print(fmt_string_hdr.format('LEN|','OCCURENCES','|NR.'))
+    print(ODDEL)
+    for wrd_len in wrd_analysis['wrd_lengths'].keys():   
+        print(fmt_string_row.format(f'{wrd_len}|', 
+            wrd_analysis['wrd_lengths'][wrd_len]*'*', 
+            f"|{wrd_analysis['wrd_lengths'][wrd_len]}"))
 
-print(ODDEL)
-print(f'Analyzuji text c. {selection}...')
-print('')
 
-#nacteni stringu k analyze, jeho ocisteni a rozdeleni do listu slov
-selection-=1
-str_in = str(TEXTS[selection]).replace('\n','').replace('\r','').replace('-',' ').strip()
-words = str_in.split(' ')
-for i in range(len(words)):
-    words[i] = words[i].strip('.,"').strip("'")
-
-#pocet slov
-N_words = len(words)
-
-#inicializace pocitadel
-N_titlecase_words = 0
-N_uppercase_words = 0
-N_lowercase_words = 0
-N_numbers = 0
-numbers_sum = 0
-
-#inicializace slovniku delek slov
-wrd_lengths=dict()
-
-#analyza textu
-for wrd in words:
-    wrd_len = len(wrd)
-    wrd_lengths[wrd_len]=wrd_lengths.setdefault(wrd_len, 0)+1
-    if wrd.istitle():
-        N_titlecase_words += 1 
-    elif wrd.isupper():
-        N_uppercase_words += 1
-    elif wrd.islower():
-        N_lowercase_words += 1
-    elif wrd.isnumeric():
-        N_numbers += 1
-        numbers_sum += int(wrd)
-
-#serazeni slovniku delek slov od nejkratsiho po nejdelsi
-wrd_lengths = dict(sorted(wrd_lengths.items()))
-
-#zobrazeni vysledku
-print(f'Pocet slov v textu: {N_words}')
-print(f'Pocet slov zacinajicich velkym pismenem: {N_titlecase_words}')
-print(f'Pocet slov psanych velkymi pismeny: {N_uppercase_words}')
-print(f'Pocet slov psanych malymi pismeny: {N_lowercase_words}')
-print(f'Pocet cisel: {N_numbers}')
-print(f'Soucet vsech cisel: {numbers_sum}')
-
-#*** Vystup do tabulky ***
-#maximalni sirka prostredniho sloupce
-max_count = max(wrd_lengths.values())
-#format radku tabulky
-fmt_string_row= "{:>4} {:<" + str(max_count) + "} {:<4}"
-#format zahlavi tabulky
-fmt_string_hdr= "{:>4} {:^" + str(max_count) + "} {:<4}"
-#vypis zahlavi a radku tabulky
-print(ODDEL)
-print(fmt_string_hdr.format('LEN|','OCCURENCES','|NR.'))
-print(ODDEL)
-for wrd_len in wrd_lengths.keys():   
-    print(fmt_string_row.format(f'{wrd_len}|',wrd_lengths[wrd_len]*'*',f'|{wrd_lengths[wrd_len]}'))
+#----------------------------------------------------------------------------        
+if __name__ == "__main__":
+    text_analyzer_main()
